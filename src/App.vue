ac588532755aa04e91960f5989f09531c90884f1
Janos Hardi <janos.hardi@gmail.com>
2023-05-11 13:48:42 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Layout from './components/Layout.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Telex.hu Zacc Layout</h1>
+  </header>
+  <main>
+    <Layout />
+  </main>
+</template>
---
+++ src/components/__tests__/Layout.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import Layout from '@/components/Layout.vue'
+
+describe('Layout', () => {
+  it('renders properly', () => {
+    const wrapper = mount(Layout)
+    expect(wrapper.text()).toContain('Test')
+  })
+})
